2069cb7738718d64ec306679ad46abc86daf27ad
unknown <rian.mizar04@gmail.com>
2024-03-10 23:48:37 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import NavigationBar from './components/NavigationBar';
 import "./style/landingPage.css";
+import React from 'react';
 
 import './App.css';
 
 function App() {
   return (
     <div>
-      {/* navbar section */}
-      <div className=''>
-        <NavigationBar/>
-      </div>
+      {/* navbar section */}      
+        <div className=''>      
+          <NavigationBar/>          
+        </div>      
       {/* navbar selesai */}
 
 
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import {Container, Navbar, Button, Row, Col} from 'react-bootstrap';
+import { Navbar, NavbarText } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 const NavigationBar = () => {
-    return (            
-        <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">Brand link</Navbar.Brand>
-        </Container>
-      </Navbar>                    
-    );
+  return (
+    <div>
+      <Navbar className="navbar">
+      <Container>
+        <Navbar.Brand href="#" className='title'>Manjo <span>Dev</span></Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <ul className='navbar-list'>
+              <li>Home</li>
+              <li>About</li>
+              <li>Project</li>
+              <li>Blog</li>
+              {/* <Button>Contact Me</Button> */}
+            </ul>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>         
+  )
 }
 
 export default NavigationBar;
